1a5348d98f6e815343b0749978f51c061d056c9d
Northy <adarsharyanmuz@gmail.com>
2022-01-21 18:34:26 +0530

    Merging files

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,43 @@
 import "./App.css";
-import Cards from "./Components/Buy_sell/Cards/Cards";
+import Cards from './Components/Buy_sell/Cards/Cards'
 import { useState } from "react";
-import LoginSignup from "./Components/LoginSignup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CarousleCards from "./Components/CarousleCards";
+import CarousleCards from "../src/Components/CarousleCards";
+import LoginSignup from "../src/Components/LoginSignup"
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 // import Navbar from "./Components/Appbar/Navbar";
 
 import Loader from "./Components/Loader/Loader";
 import Dashboard from "./Components/Dashboard/Dashboard";
-import Navbar from "./Components/Appbar/Navbar";
+import Navbar from './Components/Appbar/Navbar'
 import Bcards from "./Components/Lost_Found/Cards/Bcards";
 
+
+
 function App() {
+
+  
+    
+  
   return (
-    <div className="App">
-      {/* <Loader/> */}
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/loginsignup" exact element={<LoginSignup />}></Route>
+    <BrowserRouter>
+  <div className="App">
+    {/* <Loader/> */}
+    <Navbar />
+<Routes>
+<Route path="/loginsignup" exact element={<LoginSignup />}></Route>
           <Route path="/carousel" exact element={<CarousleCards />}></Route>
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route exact path="/buySell" element={<Cards />} />
-          <Route exact path="/lostFound" element={<Bcards />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  <Route exact path='/dashboard' element={<Dashboard/>}/>
+  <Route  exact path='/buySell'  element={<Cards/>}/>
+  <Route exact path='/lostFound'  element={<Bcards/>}/>
+    </Routes>
+  </div>
+  </BrowserRouter>
+  )
 }
 
 export default App;
