735a64af106b54a179532f919b9a46962a625956
Northy <adarsharyanmuz@gmail.com>
2022-01-31 10:45:38 +0530

    added addlostfounditem page

--- client/src/redux/actions/LostFoundActions.js
+++ client/src/redux/actions/LostFoundActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
- import {GET_ALL_LOST_FOUND_ITEMS } from '../constants/AllConstants'
+ import {GET_ALL_LOST_FOUND_ITEMS ,
+  GET_PRODUCT_DETAILS_REQUEST,
+  GET_SINGLE_LOST_FOUND_ITEM,
+  ADD_NEW_LOST_FOUND_ITEM
+} from '../constants/AllConstants'
 
  export const getAllLostFoundItems = () => async (dispatch) => {
     try {
@@ -9,6 +13,40 @@ import axios from 'axios'
       dispatch({
         type:GET_ALL_LOST_FOUND_ITEMS ,
         payload: data,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  export const getLostFoundProductDetails = (itemId) => async (dispatch) => {
+    dispatch({
+      type: GET_PRODUCT_DETAILS_REQUEST,
+      payload: true,
+    });
+    try {
+      const { data } = await axios.get(
+        `API${itemId}`
+      );
+      console.log(data);
+      dispatch({
+        type: GET_SINGLE_LOST_FOUND_ITEM,
+        payload: data,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  export const addNewLostFoundItem= (itemName,brand,category,postedBy,date,imageList)=> async (dispatch) => {
+    try {
+     const res=  await axios.post(
+        "api/newproduct",{
+          itemName,
+        }
+      );
+    
+      dispatch({
+        type: ADD_NEW_LOST_FOUND_ITEM,
+       
       });
     } catch (err) {
       console.log(err);
--- client/src/redux/reducers/LostFoundReducer.js
+++ client/src/redux/reducers/LostFoundReducer.js
@@ -1,7 +1,13 @@
-import {GET_ALL_LOST_FOUND_ITEMS } from'../constants/AllConstants'
+import {GET_ALL_LOST_FOUND_ITEMS,
+  GET_PRODUCT_DETAILS_REQUEST,
+  GET_SINGLE_LOST_FOUND_ITEM,
+ADD_NEW_LOST_FOUND_ITEM } from'../constants/AllConstants'
 
 const initialState = {
     itemList: [],
+    singleProduct: {},
+  firstImage: "",
+  isLoading: false,
   };
 
   const LostFoundReducer = (state = initialState, action) => {
@@ -11,6 +17,22 @@ const initialState = {
           ...state,
           itemList: action.payload,
         };
+        case GET_PRODUCT_DETAILS_REQUEST:
+      return {
+        ...state,
+        isLoading: action.payload,
+      };
+      case GET_SINGLE_LOST_FOUND_ITEM:
+        return {
+          ...state,
+          isLoading: false,
+          singleProduct: action.payload,
+          firstImage: action.payload.itemImages
+            ? action.payload.itemImages[0].img
+            : "",
+        };
+        case ADD_NEW_LOST_FOUND_ITEM:
+          return state;
       default:
         return state;
     }
