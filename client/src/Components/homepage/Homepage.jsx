8d8c1d13822b78421148e1e33d9adc05eb214a2d
thebeast1819 <img_2020008@iiitm.ac.in>
2022-02-14 15:03:08 +0530

    Feedback modified

--- client/src/Components/homepage/Homepage.jsx
+++ client/src/Components/homepage/Homepage.jsx
@@ -2,7 +2,7 @@ import  Nav_bar  from './nav_bar/Nav_bar';
 import  Header  from './header/Header';
 import  Feature  from './feature/Feature';
 import  Motive  from './motive/Motive';
-import  Review  from './review/Review';
+import  Feedback  from './feedback/Feedback';
 import  Footer  from './footer/Footer';
 import './homepage.css';
 
@@ -14,7 +14,7 @@ const Homepage = () => {
             <Header/>
             <Feature/>
             <Motive/>
-            <Review/>
+            <Feedback/>
             <Footer/>
         </div>
      );
