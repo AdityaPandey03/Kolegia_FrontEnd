a0a343d99f7dc67d854aabc78f3cc66ca42aca81
KislaySingh10 <singhkislay1012@gmail.com>
2022-02-19 21:49:51 +0530

    merge issue resolved

--- client/src/Pages/Buy_sell.js
+++ client/src/Pages/Buy_sell.js
@@ -46,7 +46,6 @@ const Cards = () => {
   const classes = useStyles();
   return (
     <>
-<<<<<<< HEAD
     <Navbar visibleSearch={true} presentPage="buySell" />
     {isLoading ? (
     <LoadingBox />
@@ -78,35 +77,5 @@ const Cards = () => {
 );
         }
 
-=======
-      <Navbar visibleSearch={true} />
-      {isLoading ? (
-        <LoadingBox />
-      ) : (
-        <Container className={classes.root}>
-          <Grid container spacing={3}>
-            {itemList.length > 0 ? (
-              itemList.map((item, index) => {
-                return (
-                  <Grid lg={3} sm={4} xm={12} md={4} item key={index}>
-                    <NoteCard editOption={false} data={item} />
-                  </Grid>
-                );
-              })
-            ) : (
-              <div></div>
-            )}
-          </Grid>
-          <div className="circle">
-            <FaPlusCircle onClick={toggleModal} className="btn-modal btn" />
-          </div>
-          <Modal const toggleModal={toggleModal} modal={modal} />
-        </Container>
-      )}
-      ;
-    </>
-  );
-};
->>>>>>> 8df03e8b0d50426aab089b4ac39bc83e1124f909
 
 export default Cards;
