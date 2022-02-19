744aa5bd5049dee357d3d5098d5b535075f4b751
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-02-20 00:20:10 +0530

    revised

--- socket/index.js
+++ socket/index.js
@@ -1,36 +1,31 @@
-const io = require("socket.io")(
-  8800,
-  {
-    cors: {
-      origin: "http://localhost:3001",
-    },
-  },
-  () => {
-    console.log("server started at port 8800");
-  }
-);
+// Packges Imports
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 let users = [];
 
 const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
+  !users.some(user => user.userId === userId) && users.push({ userId, socketId });
 };
 
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
+const removeUser = socketId => {
+  users = users.filter(user => user.socketId !== socketId);
 };
 
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
+const getUser = userId => {
+  return users.find(user => user.userId === userId);
 };
 
-io.on("connection", (socket) => {
+io.on("connection", socket => {
   //when user is connected
   console.log("socket server connected");
 
   //getting userid and socketid and mapping them to each other for later use.
-  socket.on("addUser", (userId) => {
+  socket.on("addUser", userId => {
     addUser(userId, socket.id);
     io.emit("getUser", users);
   });
@@ -51,4 +46,16 @@ io.on("connection", (socket) => {
     removeUser(socket.id);
     io.emit("getUser", users);
   });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use((req, res, next) => {
+  res.send("Not Found");
+});
+
+server.listen(process.env.PORT || 8800, () => {
+  console.log("listening on *:8800");
 });
