c0c045e1cc2f4d4e0bbc614c4b764bbc4ae0530f
vivek_sherkhane <sherkhanevivek@gmail.com>
2022-02-20 17:59:55 +0530

    buy sell chat

--- client/src/redux/actions/BuySellActions.js
+++ client/src/redux/actions/BuySellActions.js
@@ -11,7 +11,8 @@ import {
   CHECKING_ERROR_BUY_SELL,
   DELETE_BUY_SELL_ITEM,
   EMPTY_SEARCH_BUY_SELL,
-  SEARCH_BUY_SELL_ITEMS
+  SEARCH_BUY_SELL_ITEMS,
+  MAKE_CONNECTION,
 } from "../constants/AllConstants";
 
 export const getAllBuySellItems = () => async (dispatch) => {
@@ -23,15 +24,14 @@ export const getAllBuySellItems = () => async (dispatch) => {
     const token = localStorage.getItem("jwt");
     const decoded = jwt_decode(token);
     const { data } = await axios.get(
-      
-      "http://localhost:3000/api/v1/buy-sell-items/get-buy-sell-feed",{
+      "http://localhost:3000/api/v1/buy-sell-items/get-buy-sell-feed",
+      {
         headers: {
           authorization: `Bearer ${decoded.auth_token}`,
         },
-      
       }
     );
-console.log(data);
+    console.log(data);
     dispatch({
       type: GET_ALL_BUY_SELL_ITEMS,
       payload: data,
@@ -55,7 +55,7 @@ export const buySellSearch = (searchQuery) => async (dispatch) => {
       {
         headers: {
           authorization: `Bearer ${decoded.auth_token}`,
-        }
+        },
       }
     );
     console.log(data);
@@ -68,14 +68,13 @@ export const buySellSearch = (searchQuery) => async (dispatch) => {
   }
 };
 
-export const emptySearchBuySell = ()=>(dispatch)=>{
+export const emptySearchBuySell = () => (dispatch) => {
   dispatch({
-    type:EMPTY_SEARCH_BUY_SELL
-  })
+    type: EMPTY_SEARCH_BUY_SELL,
+  });
 };
 
 export const getSingleProductDetails = (itemId) => async (dispatch) => {
-
   dispatch({
     type: NEW_REQUEST,
     payload: true,
@@ -84,13 +83,14 @@ export const getSingleProductDetails = (itemId) => async (dispatch) => {
     const token = localStorage.getItem("jwt");
     const decoded = jwt_decode(token);
     const { data } = await axios.get(
-      `http://localhost:3000/api/v1/buy-sell-items/get-buysell-product-details?product_id=${itemId}`,{
-        headers:{
-          authorization:`Bearer ${decoded.auth_token}`,
+      `http://localhost:3000/api/v1/buy-sell-items/get-buysell-product-details?product_id=${itemId}`,
+      {
+        headers: {
+          authorization: `Bearer ${decoded.auth_token}`,
         },
       }
     );
-  
+
     dispatch({
       type: GET_SINGLE_BUY_SELL_ITEM,
       payload: data,
@@ -101,7 +101,6 @@ export const getSingleProductDetails = (itemId) => async (dispatch) => {
 };
 
 export const addNewBuySellItem = (formData) => async (dispatch) => {
- 
   try {
     var token = "";
     for (var key of formData.entries()) {
@@ -109,23 +108,27 @@ export const addNewBuySellItem = (formData) => async (dispatch) => {
         token = key[1];
       }
     }
-    const res = await axios.post("http://localhost:3000/api/v1/buy-sell-items/create-new-buysell-product", formData,{
-      headers:{
-        authorization:`Bearer ${token}`,
+    const res = await axios.post(
+      "http://localhost:3000/api/v1/buy-sell-items/create-new-buysell-product",
+      formData,
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
       }
-    });
+    );
 
     dispatch({
       type: ADD_NEW_BUY_SELL_ITEM,
-      payload:res,
+      payload: res,
     });
   } catch (error) {
     if (error.response) {
       console.log(error.response);
       dispatch({
-          type:CHECKING_ERROR_BUY_SELL,
-          payload:error.response,
-      })
+        type: CHECKING_ERROR_BUY_SELL,
+        payload: error.response,
+      });
     }
   }
 };
@@ -135,110 +138,97 @@ export const getAllOwnBuySellItems = () => async (dispatch) => {
     type: NEW_REQUEST,
     payload: true,
   });
-    try {
-      const token = localStorage.getItem("jwt");
+  try {
+    const token = localStorage.getItem("jwt");
     const decoded = jwt_decode(token);
-      const { data } = await axios.get(
-        "http://localhost:3000/api/v1/buy-sell-items/get-own-buy-sell-list",{
-          headers:{
-            authorization:`Bearer ${decoded.auth_token}`,
-          },
-        }
-        
-      );
+    const { data } = await axios.get(
+      "http://localhost:3000/api/v1/buy-sell-items/get-own-buy-sell-list",
+      {
+        headers: {
+          authorization: `Bearer ${decoded.auth_token}`,
+        },
+      }
+    );
     // console.log(data);
-      dispatch({
-        type: GET_MY_OWN_BUY_SELL_ITEMS,
-        payload: data,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  export const editBuySellItem= (formData)=> async (dispatch) => {
-    
-    
-    try {
-      var token = "";
-      for (var key of formData.entries()) {
-        if (key[0] === "token") {
-          token = key[1];
-        }
-      }
-     const res=  await axios.put(
-        "http://localhost:3000/api/v1/buy-sell-items/edit-buy-sell-product",formData
-          
-        
-          ,{
-            headers:{
-              authorization:`Bearer ${token}`,
-            }
-          }
-        
-      );
-    console.log(res)
-      dispatch({
-        type: EDIT_BUY_SELL_ITEM,
-        payload:res
-       
-      });
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response);
-        dispatch({
-            type:CHECKING_ERROR_BUY_SELL,
-            payload:error.response,
-        })
-      }
-    }
-  };
-
-  //delete
-
-  export const deleteBuySellItem= (product_id,token)=> async (dispatch) => {
-    // console.log(requirement_id,token);
-    
-      try {
-  
-       const res=  await axios.delete(
-          "http://localhost:3000/api/v1/buy-sell-items/delete-buy-sell-product",
-            
-       {
-            
-              headers:{
-                authorization:`Bearer ${token}`,
-              },
-              data:{
-                product_id,
-              },
-            }
-          
-        );
-      console.log(res);
-        dispatch({
-          type: DELETE_BUY_SELL_ITEM,
-          payload:res
-         
-        });
-      } catch (error) {
-        if (error.response) {
-          console.log(error.response);
-          dispatch({
-              type:CHECKING_ERROR_BUY_SELL,
-              payload:error.response,
-          })
-        }
-      }
-    };
-
-
- 
-  export const resetStatus=(dispatch)=>{
     dispatch({
-      type:RESET_STATUS,
+      type: GET_MY_OWN_BUY_SELL_ITEMS,
+      payload: data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const editBuySellItem = (formData) => async (dispatch) => {
+  try {
+    var token = "";
+    for (var key of formData.entries()) {
+      if (key[0] === "token") {
+        token = key[1];
+      }
     }
+    const res = await axios.put(
+      "http://localhost:3000/api/v1/buy-sell-items/edit-buy-sell-product",
+      formData,
 
-    )
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(res);
+    dispatch({
+      type: EDIT_BUY_SELL_ITEM,
+      payload: res,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response);
+      dispatch({
+        type: CHECKING_ERROR_BUY_SELL,
+        payload: error.response,
+      });
+    }
+  }
+};
 
-   };
+//delete
+
+export const deleteBuySellItem = (product_id, token) => async (dispatch) => {
+  // console.log(requirement_id,token);
+
+  try {
+    const res = await axios.delete(
+      "http://localhost:3000/api/v1/buy-sell-items/delete-buy-sell-product",
+
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+        data: {
+          product_id,
+        },
+      }
+    );
+    console.log(res);
+    dispatch({
+      type: DELETE_BUY_SELL_ITEM,
+      payload: res,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response);
+      dispatch({
+        type: CHECKING_ERROR_BUY_SELL,
+        payload: error.response,
+      });
+    }
+  }
+};
+
+export const resetStatus = (dispatch) => {
+  dispatch({
+    type: RESET_STATUS,
+  });
+};
