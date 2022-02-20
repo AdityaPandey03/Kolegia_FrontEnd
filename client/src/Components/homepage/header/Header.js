480368f83f3e171f47bbcb9eb88f0a22ce92d74b
AdityaPandey03 <pandey.aditya4272@gmail.com>
2022-02-20 22:27:51 +0530

    snapshots added

--- client/src/Components/homepage/header/Header.js
+++ client/src/Components/homepage/header/Header.js
@@ -14,7 +14,7 @@ const Header = () => (
     </div>
     <div className="header-content3">
       <h1>Hey, I am your College Mate!</h1>
-      <p>In such a big college, it's so difficult to find your pendrive if you forget it somewhere. Or, if you want to buy some old laptop because of lack of money, especially for freshers. Isn't it?</p>
+      <p>In such a big college, it's  difficult to find your pen drive if you forget it somewhere. Or, if you want to buy some old laptop because of lack of money, especially for freshers. Isn't it?</p>
       <p>Don't worry, you are at right place where you can report if you have lost something or if you want to buy/sell something.</p>
       <div className="header-content_input3">
       <a href="#feature"><p>Know More</p></a>
